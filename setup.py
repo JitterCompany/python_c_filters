b6d7abca24b484428ed4f5a9ef939364d3b931f2
Ingmar Jager <ingmarjager@gmail.com>
2016-08-30 16:15:09 +0200

    Move all 3rd party arm files to arm dir

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ import numpy as np
 module = Extension('cfilt', 
         sources = ['cfiltmodule.c', 
             'filter.c',
-            'arm_biquad_cascade_df2T_init_f32.c', 
-            'arm_biquad_cascade_df2T_f32.c',
-            'arm_biquad_cascade_df2T_init_f64.c', 
-            'arm_biquad_cascade_df2T_f64.c',
+            'arm/arm_biquad_cascade_df2T_init_f32.c', 
+            'arm/arm_biquad_cascade_df2T_f32.c',
+            'arm/arm_biquad_cascade_df2T_init_f64.c', 
+            'arm/arm_biquad_cascade_df2T_f64.c',
             ],
         library_dirs=['./build/'],
-        include_dirs = ['.', np.get_include()], 
+        include_dirs = ['.', './arm', np.get_include()], 
         extra_compile_args=['-std=c99', '-DARM_MATH_CM4', '-DTEST', '-Xlinker -dead_strip'])
 
 setup(name= 'CFILT',
